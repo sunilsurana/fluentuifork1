c8dd334059022a9127346c0eef249f19ad705b88
Sunil Surana <sunilsurana1986@gmail.com>
2022-11-12 08:45:46 +0000

    convert

--- apps/vr-tests-react-components/convertStoriesToStoryWright.sh
+++ apps/vr-tests-react-components/convertStoriesToStoryWright.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -x
-find ./src/stories/ -type f | xargs sed -i "s/import.*Screener.*screener'/import { StoryWright, Steps } from 'storywright'/g"
-find ./src/stories/ -type f | xargs sed -i "s/Screener.Steps/Steps/g"
-find ./src/stories/ -type f | xargs sed -i "s/Screener/StoryWright/g"
+find ./src -type f | xargs sed -i "s/import.*Screener.*Steps.*screener'/import { StoryWright, Steps } from 'storywright'/g"
+find ./src -type f | xargs sed -i "s/import.*Screener.*Step.*screener'/import { StoryWright, Step } from 'storywright'/g"
+find ./src -type f | xargs sed -i "s/import.*Screener from .*screener'/import { StoryWright, Steps } from 'storywright'/g"
+find ./src/stories/Button/utils.ts -type f | xargs sed -i "s/import Screener from.*screener'/import { Steps } from 'storywright'/g"
+find ./src -type f | xargs sed -i "s/Screener.Steps/Steps/g"
+find ./src -type f | xargs sed -i "s/<Screener/<StoryWright/g"
+find ./src -type f | xargs sed -i "s/Screener>/StoryWright>/g"
