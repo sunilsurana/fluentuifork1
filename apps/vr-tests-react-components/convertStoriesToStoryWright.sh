a4e1db708a68205f25b0b29bb1a9b1f64958058c
Sunil Surana <sunilsurana1986@gmail.com>
2022-11-12 09:26:05 +0000

    add regex

--- apps/vr-tests-react-components/convertStoriesToStoryWright.sh
+++ apps/vr-tests-react-components/convertStoriesToStoryWright.sh
@@ -2,14 +2,18 @@
 
 #!/usr/bin/env bash
 
+find ./src -type f | xargs sed -i "s/Screener.Steps/Steps/g"
+find ./src -type f | xargs sed -i "s/<Screener/<StoryWright/g"
+find ./src -type f | xargs sed -i "s/Screener>/StoryWright>/g"
+
 find ./src -type f |while read fname; do
   impor=""
   if grep -q "screener-storybook" $fname; then
     echo "$fname"
     if grep -q "<Screener" $fname; then
-      impor="${impor}Screener, "
+      impor="${impor}StoryWright, "
     fi
-    if grep -q "Steps" $fname; then
+    if grep -q "Steps(" $fname; then
       impor="${impor}Steps, "
     fi
     if grep "Step " $fname; then
