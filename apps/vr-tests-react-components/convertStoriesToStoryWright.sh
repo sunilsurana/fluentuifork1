720e6ecf2f251ddf1bb01b49c9d2ccfb3d7a5506
Sunil Surana <sunilsurana1986@gmail.com>
2022-11-12 09:21:31 +0000

    checkpoint

--- apps/vr-tests-react-components/convertStoriesToStoryWright.sh
+++ apps/vr-tests-react-components/convertStoriesToStoryWright.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
-set -x
-find ./src -type f | xargs sed -i "s/import.*Screener.*Steps.*screener'/import { StoryWright, Steps } from 'storywright'/g"
-find ./src -type f | xargs sed -i "s/import.*Screener.*Step.*screener'/import { StoryWright, Step } from 'storywright'/g"
-find ./src -type f | xargs sed -i "s/import.*Screener from .*screener'/import { StoryWright, Steps } from 'storywright'/g"
-find ./src/stories/Button/utils.ts -type f | xargs sed -i "s/import Screener from.*screener'/import { Steps } from 'storywright'/g"
-find ./src -type f | xargs sed -i "s/Screener.Steps/Steps/g"
-find ./src -type f | xargs sed -i "s/<Screener/<StoryWright/g"
-find ./src -type f | xargs sed -i "s/Screener>/StoryWright>/g"
+
+#!/usr/bin/env bash
+
+find ./src -type f |while read fname; do
+  impor=""
+  if grep -q "screener-storybook" $fname; then
+    echo "$fname"
+    if grep -q "<Screener" $fname; then
+      impor="${impor}Screener, "
+    fi
+    if grep -q "Steps" $fname; then
+      impor="${impor}Steps, "
+    fi
+    if grep "Step " $fname; then
+      impor="${impor}Step , "
+    fi
+  fi
+  impor="${impor%??}"
+  sed -i "s/import.*screener'/import { ${impor} } from 'storywright'/g" $fname
+done
+
